86abc621b8686d8e280c8cd5f2747915f912de74
DianaLeitao <diana.h.p.leitao@gmail.com>
2022-04-29 21:26:20 +0100

    Updated burger menu to full

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,23 +1,9 @@
-const menuBtn = document.querySelector('.menuHamburger');
-let menuOpen = false;
-
-menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-
-    }
-});
-
-function openContent() {
-  var menuList = document.getElementById("menuList");
-  if (menuList.style.display === "flex") {
-    menuList.style.display = "none";
-  } else {
-    menuList.style.display = "flex";
-  }
+document.querySelector('.burgerbtn').onclick = function (e) {
+  var menu = document.querySelector('.menu');
+  var burgerbtn = document.querySelector('.burgerbtn');
+  
+  menu.classList.toggle('is-active');
+  burgerbtn.classList.toggle('is-active');
+  
+  e.preventDefault();
 }
