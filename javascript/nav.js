a22fb6d8e910939f5f1485b70674318c37bdc03c
DianaLeitao <diana.h.p.leitao@gmail.com>
2022-05-17 21:57:25 +0100

    Responsive

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,6 +1,13 @@
 document.querySelector('.burgerbtn').onclick = function (e) {
   var menu = document.querySelector('.menu');
   var burgerbtn = document.querySelector('.burgerbtn');
+  var pagecontent = document.querySelector('.page-content');
+
+  if(pagecontent.style.visibility !=="hidden"){
+    pagecontent.style.visibility = "hidden";
+  }else {
+    pagecontent.style.visibility = "visible";
+  }
   
   menu.classList.toggle('is-active');
   burgerbtn.classList.toggle('is-active');
